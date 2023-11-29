a21254b4107ee4dd7dd1b09d39a2a19e7bc3265b
Robert Saalbach <robert@saalbach.dev>
2023-11-29 23:06:07 +0100

    added: font handling and basic css setup

--- src/main.go
+++ src/main.go
@@ -3,36 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"crypto/rand"
 
 	"github.com/gorilla/mux"
 )
 
-var secretKey []byte;
-
-var testUserName = "temp"
+// var secretKey []byte;
+var err error
 
 func main() {
 
-	secretKey = make([]byte, 32)
-	length, err := rand.Read(secretKey)
-
-	if err != nil {
-		// TODO:
-	}
-	
-	if length != 32 {
-		// TODO:
-	}
+// 	secretKey = make([]byte, 32)
+// 	length, err := rand.Read(secretKey)
+// 
+// 	if err != nil {
+// 		// TODO:
+// 	}
+// 	
+// 	if length != 32 {
+// 		// TODO:
+// 	}
 
 	mux := mux.NewRouter().StrictSlash(true)
 
 	mux.HandleFunc("/", GetMainPage)
 	mux.HandleFunc("/css/{style}", GetCSSStyle)
 	mux.HandleFunc("/js/{script}", GetJavaScript)
-	mux.HandleFunc("/recipe/{recipe}", GetRecipeArticle)
-	mux.HandleFunc("/cookies/handout-test", handoutCookieTest)
-	mux.HandleFunc("/cookies/validate-test", validateCookieTest)
+	mux.HandleFunc("/fonts/{font}", GetFontface)
+	//mux.HandleFunc("/cookies/handout-test", handoutCookieTest)
+	//mux.HandleFunc("/cookies/validate-test", validateCookieTest)
 
 	fmt.Println("Listening...")
 	err = http.ListenAndServe(":8000", mux)
@@ -42,20 +40,20 @@ func main() {
 	}
 }
 
-func handoutCookieTest(writer http.ResponseWriter, request *http.Request) {
-	cookie := GenerateLoginCookie("temp", secretKey)
-
-	http.SetCookie(writer, &cookie)
-
-	fmt.Println("Cookie Set")
-	writer.Write([]byte("Set the cookie " + (cookie.Name) + " for the user temp. The cookie value is as follows:" + string(cookie.Value) + "\n"))
-}
-
-func validateCookieTest(writer http.ResponseWriter, request *http.Request) {
-	if val, user := ValidateLoginCookie(request, "sis50.nl.login.validation", secretKey, "temp"); val {
-		fmt.Print("===================\nCookie was validated for user:", user ,"\n===================\n")
-	}
-}
+// func handoutCookieTest(writer http.ResponseWriter, request *http.Request) {
+// 	cookie := GenerateLoginCookie("temp", secretKey)
+// 
+// 	http.SetCookie(writer, &cookie)
+// 
+// 	fmt.Println("Cookie Set")
+// 	writer.Write([]byte("Set the cookie " + (cookie.Name) + " for the user temp. The cookie value is as follows:" + string(cookie.Value) + "\n"))
+// }
+// 
+// func validateCookieTest(writer http.ResponseWriter, request *http.Request) {
+// 	if val, user := ValidateLoginCookie(request, "sis50.nl.login.validation", secretKey, "temp"); val {
+// 		fmt.Print("===================\nCookie was validated for user:", user ,"\n===================\n")
+// 	}
+// }
 
 
 // TODO: this _could_ be done better, for both the CSS and JS but explicit is good in this case
@@ -73,13 +71,14 @@ func GetJavaScript(writer http.ResponseWriter, request *http.Request) {
 	http.ServeFile(writer, request, "src/static/js/" + script)
 }
 
+func GetFontface(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	font := vars["script"]
+
+	http.ServeFile(writer, request, "src/static/fonts/" + font)
+}
+
 func GetMainPage(writer http.ResponseWriter, request *http.Request) {
 	http.ServeFile(writer, request, "src/static/templates/index.html")
 }
 
-func GetRecipeArticle(writer http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
-	recipe := vars["recipe"]
-
-	http.ServeFile(writer, request, "src/static/recipes/" + recipe)
-}
