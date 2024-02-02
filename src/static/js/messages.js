0b8a2b085a771158cb322f45670284b0d83339e4
2weiEmu <saalbach.robert@outlook.de>
2024-02-02 21:41:48 +0100

    added: preliminary ** bold marking, it's definitely broken and I'm an idiot

--- src/static/js/messages.js
+++ src/static/js/messages.js
@@ -34,7 +34,7 @@ window.onload = (event) => {
 				var el = document.createElement("div")
 				el.classList.add("msg")
 				var msgPar = document.createElement("p")
-				msgPar.innerText = messagePageZero.messages[i]
+				msgPar.innerHTML = formatMessage(messagePageZero.messages[i]);
 				el.appendChild(msgPar)
 				msgList.appendChild(el)
 			}
@@ -87,7 +87,7 @@ function loadMoreItems(button, flag = false) {
 				var el = document.createElement("div")
 				el.classList.add("msg")
 				var msgPar = document.createElement("p")
-				msgPar.innerText = messagePageZero.messages[i]
+				msgPar.innerHTML = formatMessage(messagePageZero.messages[i]);
 				el.appendChild(msgPar)
 				msgList.appendChild(el)
 			}
@@ -101,4 +101,69 @@ function loadMoreItems(button, flag = false) {
 		}
 	})
 
+}
+
+var symbolMap = {
+	"*": "b",
+	"_": "i",
+	"~": "s",
+}
+
+function formatMessage(text) {
+	var open = -1;
+	var close = -1;
+
+	var closeOpenList = [];
+	var len = text.length;
+	text += " " // add one extra, for the sake of convenience. Not performant.
+
+	for (var i = 0; i < len; i++) {
+		if (text[i] == "*" && text[i+1] != " " && open == -1) {
+			open = i;
+		}
+		else if (open != -1 && text[i] == "*") {
+			close = i;
+
+			closeOpenList.push(open);
+			closeOpenList.push(close);
+		}
+	}
+
+	console.log("closeOpenList: ", closeOpenList)
+	console.log("text: ", text)
+
+	var pieces = [];
+	var prev = 0;
+	for (var i = 0; i < closeOpenList.length; i++) {
+		var pos = closeOpenList[i];
+		var slice = text.slice(prev, pos)
+		console.log("slice: ", slice, "at i: ", i)
+		pieces.push(slice)
+		prev = pos + 1;
+	}
+
+	pieces.push(text.slice(prev, text.length - 1))
+
+	console.log("pieces: ", pieces)
+
+	var openTag = true;
+	var result = "";
+	for (var i = 0; i < pieces.length - 1; i++) {
+		if (openTag) {
+			result += pieces[i] + "<b>";
+		} else {
+			result += pieces[i] + "</b>"
+		}
+		openTag = !openTag
+	}
+
+	if (closeOpenList.length != 0) {
+		result += pieces[pieces.length - 1]
+	} else {
+		result = text;
+	}
+
+	console.log("result: ", result)
+	
+	return result
 }
