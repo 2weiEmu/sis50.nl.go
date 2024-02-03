d6397247733349e86019b2d84e8801a0e9f8ffeb
2weiEmu <saalbach.robert@outlook.de>
2024-02-03 13:42:24 +0100

    updated: bolding works a _lot_ better now

--- src/static/js/messages.js
+++ src/static/js/messages.js
@@ -110,60 +110,24 @@ var symbolMap = {
 }
 
 function formatMessage(text) {
-	var open = -1;
-	var close = -1;
+	var result = ""
+	var symbolOpen = false
+	text = text + " "
 
-	var closeOpenList = [];
-	var len = text.length;
-	text += " " // add one extra, for the sake of convenience. Not performant.
+	for (var i = 0; i < text.length - 1; i++) {
 
-	for (var i = 0; i < len; i++) {
-		if (text[i] == "*" && text[i+1] != " " && open == -1) {
-			open = i;
+		if (text[i] == "*" && text[i + 1] != " " && !symbolOpen) {
+			symbolOpen = true
+			result += "<b>"
 		}
-		else if (open != -1 && text[i] == "*") {
-			close = i;
-
-			closeOpenList.push(open);
-			closeOpenList.push(close);
+		else if (text[i] == "*" && symbolOpen) {
+			symbolOpen = false
+			result += "</b>"
+		}
+		else {
+			result += text[i]
 		}
 	}
 
-	console.log("closeOpenList: ", closeOpenList)
-	console.log("text: ", text)
-
-	var pieces = [];
-	var prev = 0;
-	for (var i = 0; i < closeOpenList.length; i++) {
-		var pos = closeOpenList[i];
-		var slice = text.slice(prev, pos)
-		console.log("slice: ", slice, "at i: ", i)
-		pieces.push(slice)
-		prev = pos + 1;
-	}
-
-	pieces.push(text.slice(prev, text.length - 1))
-
-	console.log("pieces: ", pieces)
-
-	var openTag = true;
-	var result = "";
-	for (var i = 0; i < pieces.length - 1; i++) {
-		if (openTag) {
-			result += pieces[i] + "<b>";
-		} else {
-			result += pieces[i] + "</b>"
-		}
-		openTag = !openTag
-	}
-
-	if (closeOpenList.length != 0) {
-		result += pieces[pieces.length - 1]
-	} else {
-		result = text;
-	}
-
-	console.log("result: ", result)
-	
 	return result
 }
