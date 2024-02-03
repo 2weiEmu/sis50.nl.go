516d41b368cc041ef2d3827b20e5465398f089c2
2weiEmu <saalbach.robert@outlook.de>
2024-02-03 13:47:04 +0100

    updated: really simple and really sutpidyl added all the other types as well

--- src/static/js/messages.js
+++ src/static/js/messages.js
@@ -111,18 +111,36 @@ var symbolMap = {
 
 function formatMessage(text) {
 	var result = ""
-	var symbolOpen = false
+	var boldOpen = false
+	var italicOpen = false
+	var strikeOpen = false
 	text = text + " "
 
 	for (var i = 0; i < text.length - 1; i++) {
 
-		if (text[i] == "*" && text[i + 1] != " " && !symbolOpen) {
-			symbolOpen = true
+		if (text[i] == "*" && text[i + 1] != " " && !boldOpen) {
+			boldOpen = true
 			result += "<b>"
 		}
-		else if (text[i] == "*" && symbolOpen) {
-			symbolOpen = false
+		else if (text[i] == "*" && boldOpen) {
+			boldOpen = false
 			result += "</b>"
+		}
+		else if (text[i] == "_" && text[i + 1] != " " && !italicOpen) {
+			italicOpen = true
+			result += "<i>"
+		}
+		else if (text[i] == "_" && italicOpen) {
+			italicOpen = false
+			result += "</b>"
+		}
+		else if (text[i] == "~" && text[i + 1] != " " && !strikeOpen) {
+			strikeOpen = true
+			result += "<s>"
+		}
+		else if (text[i] == "~" && strikeOpen) {
+			strikeOpen = false
+			result += "</s>"
 		}
 		else {
 			result += text[i]
